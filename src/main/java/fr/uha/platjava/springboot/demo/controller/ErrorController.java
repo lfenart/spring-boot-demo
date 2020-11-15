c33e125f0f20d57fe68ee5613780863bfbab9922
JN <jn.balanche@sfr.fr>
2020-11-15 19:24:04 +0100

    error handling with thymeleaf

---
+++ src/main/java/fr/uha/platjava/springboot/demo/controller/ErrorController.java
@@ -0,0 +1,13 @@
+package fr.uha.platjava.springboot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+  @RequestMapping("/")
+  public void handleRequest() {
+      throw new RuntimeException("test exception");
+  }
+}
